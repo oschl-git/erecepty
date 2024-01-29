15a80bde1e8fa20873a6f61cfacc8c5d3a1bc0e3
Ondřej Schlaichert <schlaichert.o@gmail.com>
2024-01-29 11:53:36 +0100

    Removed almost fucking everything man

--- src/database/medicalInstitutionGateway.ts
+++
@@ -1,7 +0,0 @@
-import connection from './connection';
-
-export async function getAll() {
-	const sql = 'select * from medical_institutions;';
-	const results = await connection.promise().query(sql);
-	return results[0];
-}
--- src/database/userGateway.ts
+++
@@ -1,8 +0,0 @@
-import connection from './connection';
-
-export function getAllUsers() {
-	connection.query('select * from users;', (error, results, fields) => {
-		if (error) throw error;
-		console.log(results);
-	});
-}
